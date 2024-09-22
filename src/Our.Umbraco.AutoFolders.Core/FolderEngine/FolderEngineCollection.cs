b012a1ea5c3e10ee7d3f2b851097e99224234f7f
Luke Fisher <mail@lukefisher.me>
2024-09-22 15:24:23 +0100

    Add method to find engine by name

--- src/Our.Umbraco.AutoFolders.Core/FolderEngine/FolderEngineCollection.cs
+++ src/Our.Umbraco.AutoFolders.Core/FolderEngine/FolderEngineCollection.cs
@@ -1,7 +1,9 @@
 // Copyright 2024 Luke Fisher
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Reflection;
 using Umbraco.Cms.Core.Composing;
+using Umbraco.Extensions;
 
 namespace Our.Umbraco.AutoFolders.Core.FolderEngine;
 
@@ -10,4 +12,8 @@ public class FolderEngineCollection : BuilderCollectionBase<Type>
     public FolderEngineCollection(Func<IEnumerable<Type>> items) : base(items)
     {
     }
+    
+    public Type? FindEngineType(string name) => this
+        .FirstOrDefault(type => type.HasCustomAttribute<FolderEngineAttribute>(false) &&
+                                type.GetCustomAttribute<FolderEngineAttribute>()?.Name == name);
 }
