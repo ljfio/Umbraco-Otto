35feb92f07617f720a93aff9bdf86191a273f132
Luke Fisher <mail@lukefisher.me>
2024-09-23 11:39:26 +0100

    Delegate matching completely to the rule

--- src/Our.Umbraco.Organizers.Core/Rules/IOrganizerRule.cs
+++ src/Our.Umbraco.Organizers.Core/Rules/IOrganizerRule.cs
@@ -6,15 +6,5 @@ public interface IOrganizerRule
 {
     string Strategy { get; }
     
-    string PropertyAlias { get; }
-    
-    SortOrder SortOrder { get; }
-    
-    IEnumerable<string> ParentTypes { get; }
-    
-    IEnumerable<string> ItemTypes { get; }
-
-    string FolderType { get; }
-
     bool Matches(IContentBase entity, IContentBase parent, OrganizerMode mode);
 }
