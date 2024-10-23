af8476a9a81050a5b08893fe9e9b405e4a03b6b9
Luke Fisher <mail@lukefisher.me>
2024-10-23 10:21:38 +0100

    Add in some example rules

---
+++ src/UmbracoTestWeb/ExampleComposer.cs
@@ -0,0 +1,58 @@
+// Copyright 2024 Luke Fisher
+// SPDX-License-Identifier: Apache-2.0
+
+using Our.Umbraco.Otto.Config;
+using Our.Umbraco.Otto.Core.Config;
+using Our.Umbraco.Otto.Rules;
+using Umbraco.Cms.Core.Composing;
+using UmbracoTestWeb.Models;
+
+namespace UmbracoTestWeb;
+
+public class ExampleComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.Configure<OrganizerSettings>(options =>
+        {
+            options.Content.Rules =
+            [
+                new TaxonomyOrganizerRule
+                {
+                    PropertyAlias = "tag",
+                    ParentTypes =
+                    [
+                        Blog.ModelTypeAlias,
+                    ],
+                    ItemTypes =
+                    [
+                        Article.ModelTypeAlias,
+                    ],
+                    FolderType = BlogCategory.ModelTypeAlias,
+                },
+                new DateOrganizerRule
+                {
+                    PropertyAlias = "updateDate",
+                    ParentTypes =
+                    [
+                        Events.ModelTypeAlias,
+                    ],
+                    ItemTypes = [
+                    
+                        Event.ModelTypeAlias,
+                    ],
+                    Year =
+                    {
+                        FolderType = EventYear.ModelTypeAlias,
+                        CreateFolder = true,
+                    },
+                    Month =
+                    {
+                        FolderType = EventMonth.ModelTypeAlias,
+                        CreateFolder = true,
+                    },
+                },
+            ];
+        });
+    }
+}
